1e4b82f90a3c4d8e5695918306067a43879a2424
Betancourt10 <Felibetan2005@gmail.com>
2024-05-20 09:52:04 -0500

    CLASE POKEMÓN
    Está completa, falta completar el cuerpo de los 3 métodos: atacar, recibirDaño y entrenar.
    Cuando se terminen las otras clase.

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,74 @@
 package simulador.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public abstract class Pokemon { 
+    //O(1)
+    private String nombre; 
+    private int salud; 
+    private int puntosDeAtaque; 
+    private TipoPokemon tipo; 
+    private Estado estado; 
+
+    private Pokemon() {//O(1)
+    }
+    //O(1)
+    public Pokemon(String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo, String estado) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
+    
+    public void atacar(Pokemon oponente){ //O(1)
+        
+    }
+    
+    public void recibirDaño(int daño) { //O(1) 
+        
+    }
+    
+    public void entrenar(){  //O(1) 
+        
+    }
+
+    public String getNombre() { //O(1)
+        return nombre;
+    }
+
+    public int getSalud() { //O(1)
+        return salud;
+    }
+
+    public int getPuntosDeAtaque() { //O(1)
+        return puntosDeAtaque; 
+    }
+
+    public TipoPokemon getTipoPokemon() { //O(1)
+        return tipo;
+    }
+
+    public String getEstado() { //O(1)
+        return estado;
+    }
+
+    public void setNombre(String nombre) { //O(1)
+        this.nombre = nombre;
+    }
+
+    public void setSalud(int salud) { //O(1)
+        this.salud = salud;
+    }
+
+    public void setPuntosDeAtaque(int puntosDeAtaque) { //O(1)
+        this.puntosDeAtaque = puntosDeAtaque;
+    }
+
+    public void setTipo(TipoPokemon tipo) { //O(1)
+        this.tipo = tipo;
+    }
+
+    public void setEstado(String estado) { //O(1)
+        this.estado = estado;
+    }
+    
 }
