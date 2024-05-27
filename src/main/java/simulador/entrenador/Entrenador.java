587ea6c75788cc717fb4f88037a8f4102b9e8a35
Nick3030w <niconianessu@gmail.com>
2024-05-26 20:46:41 -0500

    Se crea la clase Entrenador con todos sus métodos

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,64 @@
 package simulador.entrenador;
+import java.util.List;
 
-public class Entrenador {
-    // TODO: Aquí va tu código
+import simulador.pokemon.Pokemon;
+
+public class Entrenador{
+    private String nombre;
+    private List<Pokemon> pokemones;
+    
+    public Entrenador(String nombre,List<Pokemon> pokemones){
+        this.nombre=nombre;
+        this.pokemones=pokemones;
+                
+    }
+    public void agregarPokemon(Pokemon pokemon) {
+        if (pokemones.size() < 6) {
+            pokemones.add(pokemon);
+        }
+    }
+    public void entrenarPokemon(Pokemon pokemon) {
+        if (pokemones.contains(pokemon)) {
+            pokemon.entrenar();           
+        } 
+    }
+    public void mostrarPokemones() {
+        if (!pokemones.isEmpty()) { //si la lista no esta vacia entonces..
+            System.out.println("Pokémones en el equipo:");
+            for (int i = 0; i < pokemones.size(); i++) {
+                Pokemon pokemon = pokemones.get(i);
+                System.out.println(pokemon);
 }
+        } else {
+            System.out.println("El equipo no tiene Pokémones.");
+        }
+    }
+     public Pokemon prepararBatalla() {
+        if (!pokemones.isEmpty()) { //saber si la lista de pokemones no esta vacia
+            Pokemon pokemonParaBatalla = pokemones.get(0); // Selecciona el primer Pokémon del equipo
+            System.out.println(pokemonParaBatalla + " ha sido seleccionado para la batalla.");
+            return pokemonParaBatalla;
+        } else {
+            System.out.println("No hay Pokémones en el equipo para participar en la batalla.");
+            return null;
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Pokemon> getPokemones() {
+        return pokemones;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPokemones(List<Pokemon> pokemones) {
+        this.pokemones = pokemones;
+    }
+     
+    }
+    
