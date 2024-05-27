721978bc6f9958227d67132128167bdc3f168950
Nick3030w <niconianessu@gmail.com>
2024-05-26 21:28:32 -0500

    CLASE BATALLA
    se crea la clase batalla con sus metodos, falta preguntar cosas sobre esta clase, y los mensajes en consola.

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,34 @@
 package simulador.batalla;
 
+import simulador.pokemon.Pokemon;
+
 public class Batalla {
-    // TODO: Aquí va tu código
+    public void iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2) {
+        System.out.println("¡La batalla entre " + pokemon1.getNombre() + " y " + pokemon2.getNombre() + " ha comenzado!");
+
+        while (pokemon1.getSalud() > 0 && pokemon2.getSalud() > 0) {
+            // El primer Pokémon ataca al segundo
+            atacar(pokemon1, pokemon2);
+            if (pokemon2.getSalud() <= 0) {
+                System.out.println(pokemon2.getNombre() + " ha sido derrotado. " + pokemon1.getNombre() + " es el ganador.");
+                return;
+            }
+
+            // El segundo Pokémon ataca al primero
+            atacar(pokemon2, pokemon1);
+            if (pokemon1.getSalud() <= 0) {
+                System.out.println(pokemon1.getNombre() + " ha sido derrotado. " + pokemon2.getNombre() + " es el ganador.");
+                return;
+            }
+        }
+    }
+    public int atacar(Pokemon atacante, Pokemon defensor) {
+        int daño = atacante.getPuntosDeAtaque() - defensor.getSalud();
+        if (daño < 0) {
+            daño = 0;
+        }
+        defensor.setSalud(defensor.getSalud() - daño);
+        return daño;
+    }
 }
+
