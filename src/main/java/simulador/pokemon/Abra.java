2657a2553a8e5cc76c5ad7bc14e6bbb76d4db185
Nick3030w <niconianessu@gmail.com>
2024-05-28 19:21:44 -0500

    ABRA
    Se arreglo un error de abra

--- src/main/java/simulador/pokemon/Abra.java
+++ src/main/java/simulador/pokemon/Abra.java
@@ -1,10 +1,8 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package simulador.pokemon;
-
-
-public class Abra {
-    
-}
 public class Abra extends Pokemon {
     public Abra() {
         super("Abra", 25, 20, TipoPokemon.PSIQUICO, Estado.NORMAL);
